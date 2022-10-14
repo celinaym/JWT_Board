ae671f88781a28c30bc9b71df7b5b7c8b04cfe29
first_commit <>
2022-10-14 15:42:54 +0900

    Intermediate skeleton code

---
+++ src/main/java/com/example/intermediate/controller/response/ResponseDto.java
@@ -0,0 +1,22 @@
+package com.example.intermediate.controller.response;
+
+import com.example.intermediate.domain.Error;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ResponseDto<T> {
+  private boolean success;
+  private T data;
+  private Error error;
+
+  public static <T> ResponseDto<T> success(T data) {
+    return new ResponseDto<>(true, data, null);
+  }
+
+  public static <T> ResponseDto<T> fail(String code, String message) {
+    return new ResponseDto<>(false, null, new Error(code, message));
+  }
+
+}
---
+++ src/main/java/com/example/intermediate/domain/Error.java
@@ -0,0 +1,20 @@
+package com.example.intermediate.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Error {
+
+    private String field;
+
+    private String message;
+
+}
